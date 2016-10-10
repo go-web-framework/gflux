e5b8f49e40310cda8e574cfc4d7611b63c5fa2d2
rahuljaisimha <rahul@jaisimha.com>
2016-10-10 11:03:50 -0700

    added some comments to the example usage

--- gorm/gorm_example.go
+++ gorm/gorm_example.go
@@ -27,13 +27,13 @@ func main() {
 	})
 	
 	// get item from database
-	product := db.Get("Code", "L1212")
+	product := db.Get("Code", "L1212") // returns a Product*
 	
 	// update the obtained item
-	db.Update(product, "Price", 2000)
+	db.Update(product, "Price", 2000) // argument is a Product*
 	
 	// delete an item
-	db.Delete(product)
+	db.Delete(product) // argument is a Product*
 }
 	
 	
