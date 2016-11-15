9d66b2f3dc6cab44107b146f1f39d03e170bd634
rahuljaisimha <rahul@jaisimha.com>
2016-11-14 18:33:19 -0600

    Commented out apimain.go

--- apimain.go
+++ apimain.go
@@ -1,20 +1,20 @@
-package main
+//package main
 
-import (
-    "github.com/go-web-framework/gflux/api"
-)
+//import (
+//    "github.com/go-web-framework/gflux/api"
+//)
 
-type Post struct {
-    Title string
-    Author string
-}
+//type Post struct {
+//    Title string
+//    Author string
+//}
 
-func main() {
-    a := api.New("sqlite3", "test.db")
-    //	a := api.New("mysql", "user:password@/test")
-    defer a.Close()
+//func main() {
+//    a := api.New("sqlite3", "test.db")
+//    //	a := api.New("mysql", "user:password@/test")
+//    defer a.Close()
 
-    a.NewResource("posts", &Post{})
+//    a.NewResource("posts", &Post{})
 
-    a.Serve()
-}
+//    a.Serve()
+//}
